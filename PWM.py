5d701c680c75afee89824c9918283c699d5b4224
Shirshendu31 <91413403+Shirshendu31@users.noreply.github.com>
2022-06-09 21:37:24 +0530

    Create PWM.py

---
+++ PWM.py
@@ -0,0 +1,6 @@
+from machine import Pin, PWM from time import sleep
+frequency = 5000
+led = PWM(Pin(14), frequency)
+while True:
+for duty_cycle in range(0, 1024):
+led.duty(duty_cycle) print(duty_cycle) sleep(0.01)
